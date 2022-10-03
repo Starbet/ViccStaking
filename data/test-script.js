eb8a5a19ee0d5c73da6862ac4f1c511ceffa94e5
Starbet <starbetdapp@gmail.com>
2022-10-03 18:44:25 +0000

    Delete data directory

--- data/test-script.js
+++
@@ -1,7 +0,0 @@
-let vicc = await ViccToken.deployed();
-let staking = await ViccStaking.deployed();
-let accounts = await web3.eth.getAccounts();
-let rewardFromFee = await staking.getUserRewardFromFee(accounts[0]);
-rewardFromFee.toString();
-vicc.approve(staking.address, "1000000000000000000000");
-staking.invest("0x0000000000000000000000000000000000000000", "1000000000000000000000");
